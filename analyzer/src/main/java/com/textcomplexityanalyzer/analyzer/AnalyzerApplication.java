3e629ff2bc4aa20b7ed6c585bb08b58c2352b731
Alena Iksanova <iksanovaal@yandex.ru>
2021-03-22 18:18:30 +0500

    Added Gate, jape patterns and Gate initialization

--- analyzer/src/main/java/com/textcomplexityanalyzer/analyzer/AnalyzerApplication.java
+++ analyzer/src/main/java/com/textcomplexityanalyzer/analyzer/AnalyzerApplication.java
@@ -1,7 +1,17 @@
 package com.textcomplexityanalyzer.analyzer;
 
+import gate.CorpusController;
+import gate.Gate;
+import gate.util.GateException;
+import gate.util.GateRuntimeException;
+import gate.util.persistence.PersistenceManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Optional;
 
 @SpringBootApplication
 public class AnalyzerApplication {
@@ -10,4 +20,14 @@ public class AnalyzerApplication {
 		SpringApplication.run(AnalyzerApplication.class, args);
 	}
 
+    @Bean
+    public CorpusController corpusController() throws GateException, URISyntaxException, IOException {
+        Gate.init();
+
+        var resource = Optional.ofNullable(getClass().getClassLoader().getResource("gate/corpus-pipeline.xml"))
+                .orElseThrow(() -> new GateRuntimeException("GATE program not found"));
+
+        return (CorpusController) PersistenceManager.loadObjectFromUrl(resource);
+    }
 }
+
--- analyzer/src/main/java/com/textcomplexityanalyzer/analyzer/controllers/MainController.java
+++ analyzer/src/main/java/com/textcomplexityanalyzer/analyzer/controllers/MainController.java
@@ -1,24 +1,50 @@
 package com.textcomplexityanalyzer.analyzer.controllers;
 
+
+import com.textcomplexityanalyzer.analyzer.models.Text;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
 
 @Controller
 public class MainController {
+    @Value("${upload.path")
+    private String uploadPath;
+
+    public Text text = new Text();
 
     @GetMapping("/")
-    public String main(/*@RequestParam(name="name", required=false, defaultValue="World") String name, */Model model) {
-        //model.addAttribute("name", name);
-        model.addAttribute("title", "Text complexity analyzer");
+    public String main(Model model) {
+        model.addAttribute("text", text.getText());
         return "main";
     }
 
     @GetMapping("/about")
     public String about(Model model) {
-        model.addAttribute("title", "Text complexity analyzer");
+        model.addAttribute("text", text.getText());
         return "about";
     }
+
+    @PostMapping("/")
+    public String analyze(@RequestParam String userText, Model model) {
+        text = new Text(userText);
+        model.addAttribute("text", text.getText());
+        return "main";
+    }
+
 
 }
